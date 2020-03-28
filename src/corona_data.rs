a005060384e23b40a2c71183d51cb8de1c5b38fa
darrexx <darx.dev@gmx.de>
2020-03-28 14:40:43 +0100

    Group Locations before entering graphite

--- src/corona_data.rs
+++ src/corona_data.rs
@@ -1,27 +1,24 @@
 
-pub mod corona_data{
-    use serde::Deserialize;
+use serde::Deserialize;
 
-    #[derive(Deserialize, Debug)]
-    pub struct Location{
-        id: usize,
-        pub country: String,
-        pub country_code: String,
-        pub country_population: Option<u32>,
-        pub province: String,
-        pub latest: LatestCoronaData,
-    }
-    
-    #[derive(Deserialize,Debug)]
-    pub struct LatestCoronaData{
-        pub confirmed : i32, //possible -1!
-        pub deaths: i32,
-        pub recovered: i32,
-    }
-    
-    #[derive(Deserialize)]
-    pub struct CoronaResponse{
-        pub locations: Vec<Location>
-    }
+#[derive(Deserialize, Debug, Clone)]
+pub struct Location{
+    id: usize,
+    pub country: String,
+    pub country_code: String,
+    pub country_population: Option<u32>,
+    pub province: String,
+    pub latest: LatestCoronaData,
+}
 
+#[derive(Deserialize,Debug, Clone)]
+pub struct LatestCoronaData{
+    pub confirmed : isize, //possible -1!
+    pub deaths: isize,
+    pub recovered: isize,
+}
+
+#[derive(Deserialize)]
+pub struct CoronaResponse{
+    pub locations: Vec<Location>
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@ mod corona_data;
 
 use reqwest;
 use dipstick::{Prefixed, Graphite, Input, InputScope};
-use corona_data::corona_data::{CoronaResponse, Location};
+use corona_data::{CoronaResponse, Location};
+use itertools::Itertools;
+
+#[derive(Debug)]
+struct CoronaMetricData{
+    country_code: String,
+    confirmed: usize,
+    deaths: usize,
+    recovered: usize,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
@@ -11,50 +20,56 @@ async fn main() -> Result<(), reqwest::Error> {
         .named("corona");
 
     let body = reqwest::get("https://coronavirus-tracker-api.herokuapp.com/v2/locations")
-    .await?
-    .text()
-    .await?;
+        .await?
+        .text()
+        .await?;
 
     let corona: CoronaResponse = serde_json::from_str(&body).unwrap();
-
+    
     let locations = corona.locations;
-    write_location_data(locations, metrics).unwrap();
+    let metric_data = preapre_data(&locations);
+
+
+
+    write_location_data(metric_data, metrics).unwrap();
     Ok(())
 }
 
-fn write_location_data(locations: Vec<Location>, metrics: Graphite) -> Result<(), String> {
-    for location in locations.iter(){
-        if location.country_code == "DE" || location.country_code == "US" || location.country_code == "DK" || location.country_code == "CR" {
-            println!("{}", location.country_code);
-            println!("{:?}", location);
-        }
-        let confirmed = if location.latest.confirmed < 0 {
-            0
-        }
-        else{
-            location.latest.confirmed
-        };
-        let deaths = if location.latest.deaths < 0 {
-            0
-        }
-        else{
-            location.latest.deaths
-        };
-        let recovered = if location.latest.recovered < 0 {
-            0
-        }
-        else{
-            location.latest.recovered
-        };
-        
-        let location_metrics = metrics.add_name(format!("{}", location.country_code)).metrics();
+fn preapre_data(locations: &Vec<Location>) -> Vec<CoronaMetricData>{
 
-        location_metrics.counter("confirmed").count(confirmed as usize);
-        std::thread::sleep(std::time::Duration::from_millis(10));
-        location_metrics.counter("deaths").count(deaths as usize);
-        std::thread::sleep(std::time::Duration::from_millis(10));
-        location_metrics.counter("recovered").count(recovered as usize);
-        std::thread::sleep(std::time::Duration::from_millis(500));
+    locations.into_iter()
+        .group_by(|x| x.country_code.clone()).into_iter()
+        .map(|(x, y) | {
+            let location = y.cloned().fold1(
+                |mut acc, metric| {
+                    acc.latest.confirmed += metric.latest.confirmed.max(0);
+                    acc.latest.recovered += metric.latest.confirmed.max(0);
+                    acc.latest.deaths += metric.latest.confirmed.max(0);
+                    acc
+                }
+            ).unwrap();
+            return CoronaMetricData{country_code : x,
+                confirmed : location.latest.confirmed as usize,
+                deaths : location.latest.deaths as usize,
+                recovered : location.latest.recovered as usize,
+            }}).collect()
+}
+
+
+fn write_location_data(data: Vec<CoronaMetricData>, graphite: Graphite) -> Result<(), String> {
+    let metrics = graphite.metrics();
+    for metric in data.iter(){
+
+        let location_metrics = metrics.add_name(format!("{}", metric.country_code));
+
+        location_metrics.counter("confirmed").count(metric.confirmed );
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+
+        location_metrics.counter("deaths").count(metric.deaths);
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+        
+        location_metrics.counter("recovered").count(metric.recovered);
+        std::thread::sleep(std::time::Duration::from_millis(1000));
     }
     Ok(())
 }
